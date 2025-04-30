5faa25dc2f6c8c6a8aabde008b38c46af7433b27
Võ Viết Trường <voviettruong141003@gmail.com>
2025-04-30 14:58:18 +0700

    done action, need review

--- client/components/posts/post-card.tsx
+++ client/components/posts/post-card.tsx
@@ -1,11 +1,10 @@
-"use client";
+'use client';
 
-import type React from "react";
+import type React from 'react';
 
-import { useState } from "react";
-import Image from "next/image";
-import Link from "next/link";
-import { formatDistanceToNow } from "date-fns";
+import { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 import {
   Heart,
   MessageCircle,
@@ -13,40 +12,44 @@ import {
   MoreHorizontal,
   Pencil,
   Trash2,
-} from "lucide-react";
-import { Button } from "@/components/ui/button";
+} from 'lucide-react';
+import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { useLanguage } from "@/components/language-provider";
-import ImageList from "./image-list";
-import { IThread } from "@/interfaces/thread";
-import { useRouter } from "next/navigation";
-import { useUserContext } from "@/contexts/userContext";
-import CreatePostDialog from "./create-post-dialog";
-import DeletePostDialog from "./delete-confirm-dialog";
-import { useFormatTime } from "@/utils/myFormatDistanceToNow";
+} from '@/components/ui/dropdown-menu';
+import { useLanguage } from '@/components/language-provider';
+import ImageList from './image-list';
+import { IThread } from '@/interfaces/thread';
+import { useRouter } from 'next/navigation';
+import { useUserContext } from '@/contexts/userContext';
+import CreatePostDialog from './create-post-dialog';
+import DeletePostDialog from './delete-confirm-dialog';
+import { useFormatTime } from '@/utils/myFormatDistanceToNow';
+import { useLikeThread, useRethread } from '@/hooks/api/use-action';
 
 interface PostCardProps {
   post: IThread;
 }
 
 export default function PostCard({ post }: PostCardProps) {
+  const [thread, setThread] = useState<IThread>(post);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
   const [isTranslated, setIsTranslated] = useState(false);
-  const [isLiked, setIsLiked] = useState(post.isLiked);
-  const [isReposted, setIsReposted] = useState(post.isReThreaded);
+  const [isLiked, setIsLiked] = useState(thread.isLiked);
+  const [isReposted, setIsReposted] = useState(thread.isReThreaded);
   const { t } = useLanguage();
   const router = useRouter();
   const { user } = useUserContext();
-  const isOwnPost = user?.username === post.user.username;
+  const isOwnPost = user?.username === thread.user.username;
   const { formatTimeToNow } = useFormatTime();
-  const formattedDate = formatTimeToNow(new Date(post.createdAt));
-
+  const formattedDate = formatTimeToNow(new Date(thread.createdAt));
+  const { mutate: likeThread, isPending: isLiking } = useLikeThread();
+  const { mutate: repostThread, isPending: isReposting } = useRethread();
+  // const
   // Determine if post is in a different language than current UI
   // const isDifferentLanguage = post.language && post.language !== language;
 
@@ -62,46 +65,77 @@ export default function PostCard({ post }: PostCardProps) {
 
   const handleDirectDetail = (e: React.MouseEvent) => {
     e.stopPropagation();
-    router.push(`/user/thread/${post._id}`);
+    router.push(`/user/thread/${thread._id}`);
+  };
+
+  const handleLike = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    setIsLiked(!isLiked);
+    likeThread(thread._id, {
+      onSuccess: (data) => {
+        // Update the thread's like count
+        setIsLiked(!isLiked);
+        setThread(data);
+      },
+      onError: (error) => {
+        console.error('Failed to like/unlike thread:', error);
+      },
+    });
+  };
+
+  const handleRepost = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    console.log('Repost clicked');
+    setIsReposted(!isReposted);
+    repostThread(thread._id, {
+      onSuccess: (data: IThread) => {
+        // Update the thread's repost count
+        setIsReposted(data.isReThreaded);
+        setThread(data);
+      },
+      onError: (error) => {
+        console.error('Failed to repost/unrepost:', error);
+      },
+    });
   };
 
   return (
     <div className="rounded-lg border bg-card p-4 shadow-sm">
       <div className="flex items-start space-x-4">
         {/* Avatar */}
-        <Link href={`/user/profile/${post.user.username}`}>
+        <Link href={`/user/profile/${thread.user.username}`}>
           <Image
-            src={post.user.avatar || "/placeholder.svg"}
-            alt={post.user.name}
+            src={thread.user.avatar || '/placeholder.svg'}
+            alt={thread.user.name}
             width={40}
             height={40}
             className="rounded-full"
           />
         </Link>
 
-        {/* Post Content */}
+        {/* thread Content */}
         <div className="flex-1 space-y-2 overflow-hidden">
           {/* Author and Date */}
           <div className="flex items-center justify-between ">
             <div>
               <Link
-                href={`/user/profile/${post.user.username}`}
+                href={`/user/profile/${thread.user.username}`}
                 className="font-medium hover:underline"
               >
-                {post.user.name}
+                {thread.user.name}
               </Link>
               <span className="text-muted-foreground">
-                {" "}
-                @{post.user.username}
+                {' '}
+                @{thread.user.username}
               </span>
               <span className="text-muted-foreground text-sm">
-                {" "}
+                {' '}
                 · {formattedDate}
               </span>
-              {post.updatedAt !== post.createdAt && (
+              {thread.updatedAt !== thread.createdAt && (
                 <span className="text-muted-foreground text-xs">
-                  {" "}
-                  ({t("edit")})
+                  {' '}
+                  ({t('edit')})
                 </span>
               )}
             </div>
@@ -121,35 +155,37 @@ export default function PostCard({ post }: PostCardProps) {
                     {isTranslated ? 'Show original' : t('translate')}
                   </DropdownMenuItem>
                 )} */}
-                {post.updatedAt && (
-                  <DropdownMenuItem>{t("viewEditHistory")}</DropdownMenuItem>
+                {thread.updatedAt && (
+                  <DropdownMenuItem>{t('viewEditHistory')}</DropdownMenuItem>
                 )}
-                <DropdownMenuItem>{t("reportContent")}</DropdownMenuItem>
+                <DropdownMenuItem>{t('reportContent')}</DropdownMenuItem>
                 {isOwnPost && (
                   <>
                     <CreatePostDialog
-                      key={post._id}
+                      key={thread._id}
                       trigger={
                         <DropdownMenuItem
+                          disabled={isLiking || isReposting}
                           onSelect={(e) => {
                             e.preventDefault(); // Ngăn dropdown tự động đóng
                           }}
                         >
                           <Pencil className="mr-2 h-4 w-4" />
-                          {t("edit")}
+                          {t('edit')}
                         </DropdownMenuItem>
                       }
-                      initialThread={post}
+                      initialThread={thread}
                       isEditing={true}
                       setIsDropdownOpen={setIsDropdownOpen}
                     />
 
                     <DropdownMenuItem
                       onClick={handleDelete}
+                      disabled={isLiking || isReposting}
                       className="text-destructive"
                     >
                       <Trash2 className="mr-2 h-4 w-4" />
-                      {t("delete")}
+                      {t('delete')}
                     </DropdownMenuItem>
                   </>
                 )}
@@ -162,10 +198,10 @@ export default function PostCard({ post }: PostCardProps) {
               <p className="text-sm">
                 {
                   // isTranslated
-                  //   ? TRANSLATIONS[post.id as keyof typeof TRANSLATIONS] ||
-                  //     post.content
+                  //   ? TRANSLATIONS[thread.id as keyof typeof TRANSLATIONS] ||
+                  //     thread.content
                   //   :
-                  post.content
+                  thread.content
                 }
               </p>
 
@@ -192,7 +228,7 @@ export default function PostCard({ post }: PostCardProps) {
             <div className="overflow-x-auto mt-2 pb-2 no-scrollbar">
               <div className="flex gap-2 w-max">
                 <ImageList
-                  files={post.media.map((img) => ({
+                  files={thread.media.map((img) => ({
                     url: img.url,
                   }))}
                   isEditable={false}
@@ -205,15 +241,16 @@ export default function PostCard({ post }: PostCardProps) {
           {/* Actions */}
           <div className="flex items-center space-x-4 pt-2">
             <Button
-              variant="ghost"
+              disabled={isLiking || isReposting}
+              onClick={handleLike}
+              variant="action"
               size="sm"
               className={`flex items-center space-x-1 ${
-                post.isLiked ? "text-red-500" : ""
+                thread.isLiked ? 'text-red-500' : ''
               }`}
-              // onClick={handleLike}
             >
-              <Heart size={18} className={isLiked ? "fill-red-500" : ""} />
-              <span>{post.reactionNum}</span>
+              <Heart size={18} className={`${isLiked ? 'fill-red-500' : ''}`} />
+              <span>{thread.reactionNum}</span>
             </Button>
             <Button
               variant="ghost"
@@ -222,28 +259,29 @@ export default function PostCard({ post }: PostCardProps) {
               onClick={handleDirectDetail}
             >
               <MessageCircle size={18} />
-              <span>{post.commentNum}</span>
+              <span>{thread.commentNum}</span>
             </Button>
             <Button
-              variant="ghost"
+              variant="action"
               size="sm"
               className={`flex items-center space-x-1 ${
-                isReposted ? "text-green-500" : ""
+                isReposted ? 'text-green-500' : ''
               }`}
-              // onClick={handleRepost}
+              onClick={handleRepost}
+              disabled={isLiking || isReposting}
             >
               <Repeat
                 size={18}
-                className={isReposted ? "fill-green-500" : ""}
+                className={isReposted ? 'fill-green-500' : ''}
               />
-              <span>{post.sharedNum}</span>
+              <span>{thread.sharedNum}</span>
             </Button>
           </div>
         </div>
       </div>
       {/* Delete confirmation dialog */}
       <DeletePostDialog
-        thread={post}
+        thread={thread}
         isOpen={isDeleteDialogOpen}
         onClose={() => setIsDeleteDialogOpen(false)}
       />
--- client/hooks/api/use-action.ts
+++ client/hooks/api/use-action.ts
@@ -1,8 +1,9 @@
-import { useUserContext } from "@/contexts/userContext";
-import { client } from "@/shared/axiosClient";
-import { queryClient } from "@/shared/queryClient";
-import { useMutation } from "@tanstack/react-query";
-import { AxiosError } from "axios";
+import { useUserContext } from '@/contexts/userContext';
+import { IThread } from '@/interfaces/thread';
+import { client } from '@/shared/axiosClient';
+import { queryClient } from '@/shared/queryClient';
+import { useMutation } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 
 interface FollowTriggerResponse {
   following: {
@@ -22,14 +23,14 @@ export const useFollowTrigger = () => {
     },
     onSuccess: (_, username) => {
       queryClient.invalidateQueries({
-        queryKey: ["userProfile", username],
+        queryKey: ['userProfile', username],
       });
       queryClient.invalidateQueries({
-        queryKey: ["userProfile", user!.username],
+        queryKey: ['userProfile', user!.username],
       });
     },
     onError: (error) => {
-      console.error("Failed to follow/unfollow user:", error);
+      console.error('Failed to follow/unfollow user:', error);
     },
   });
 };
@@ -51,12 +52,47 @@ export const useRemoveFollower = () => {
     },
     onSuccess: (_, username) => {
       queryClient.invalidateQueries({
-        queryKey: ["userProfile", username],
+        queryKey: ['userProfile', username],
       });
       queryClient.invalidateQueries({
-        queryKey: ["userProfile", user!.username],
+        queryKey: ['userProfile', user!.username],
       });
     },
     onError: (error: any) => {},
+  });
+};
+
+export const useLikeThread = () => {
+  return useMutation<IThread, AxiosError, string>({
+    mutationFn: async (threadId: string) => {
+      const response = await client.post(`/action/like/${threadId}`);
+      return response.data;
+    },
+    onSuccess: (_, threadId) => {
+      // Invalidate thread data to refresh UI
+      queryClient.invalidateQueries({
+        queryKey: ['threadDetail', threadId],
+      });
+    },
+    onError: (error) => {
+      console.error('Failed to like/unlike thread:', error);
+    },
+  });
+};
+
+export const useRethread = () => {
+  return useMutation<IThread, AxiosError, string>({
+    mutationFn: async (threadId: string) => {
+      const response = await client.post(`/action/rethread/${threadId}`);
+      return response.data;
+    },
+    onSuccess: (_, threadId) => {
+      queryClient.invalidateQueries({
+        queryKey: ['threadDetail', threadId],
+      });
+    },
+    onError: (error) => {
+      console.error('Failed to rethread/unrethread thread:', error);
+    },
   });
 };
